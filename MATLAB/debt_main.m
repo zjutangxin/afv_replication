27a75e04a5246ca4437a1f977f192d1a8200b5e8
Xin Tang <zjutangxin@gmail.com>
2019-11-11 15:57:05 -0500

    debug fixed demand

---
+++ MATLAB/debt_main.m
@@ -0,0 +1,20 @@
+% -------------------------------------------------------------------------
+%                      Program Description
+% -------------------------------------------------------------------------
+%   
+% Purpose:
+%     - Main program for one country with fixed foreign demand
+%     - Saving Glut Project
+%  
+% Author:
+%     - Xin Tang @ International Monetary Fund
+%  
+% Record of Revisions:
+%         Date:                 Description of Changes
+%     ============        =================================
+%      11/11/2019                 Original Version
+% =========================================================================
+clc;
+clear all;
+
+% Data Dictionary
